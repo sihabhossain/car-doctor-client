467c8f12169453e97f8c493fc80b6a0cac9e9c29
Sihab Hossain <sihabhossain380@gmail.com>
2023-05-09 23:41:37 +0600

    video 4 done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -8,9 +8,26 @@ import img6 from "../../../assets/images/banner/6.jpg";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full h-[600px]">
       <div id="slide1" className="carousel-item relative w-full">
-        <img src={img1} className="w-full" />
+        <img src={img1} className="w-full rounded-xl" />
+        <div className="absolute h-full left-0 top-0 flex items-center bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)] rounded-xl">
+          <div className="text-white space-y-7 pl-12 w-1/2">
+            <h2 className="text-6xl font-bold ">
+              Affordable Price For Car Servicing
+            </h2>
+            <p>
+              There are many variations of passages available , but the majority
+              have suffered alertation in some form
+            </p>
+            <div>
+              <button className="btn btn-primary mr-5">Discover more</button>
+              <button class="btn btn-outline btn-secondary">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide4" className="btn btn-circle mr-5">
             ❮
